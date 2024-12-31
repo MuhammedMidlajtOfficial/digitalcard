937811a5a4ce181a22d89740c61c4dec9eca6b3b
MidlajLevonTechno <muhammedmidlaj236@gmail.com>
2024-12-31 14:30:56 +0530

    ticket system update

--- src/application/Components/SupportTicketSystem/ViewAndRespondTicket/Resolved.jsx
+++ src/application/Components/SupportTicketSystem/ViewAndRespondTicket/Resolved.jsx
@@ -5,27 +5,43 @@ import john_img from "../../../Assets/Images/John_img.png";
 import { useNavigate } from "react-router-dom";
 import { Spin, Divider, Flex } from "antd"; // Import Spin for loading indicator
 import { useLoading } from "../../../Services/loadingService";
+import axiosInstance from "../../../../AxiosConfig";
+import axiosInstanceForTicket from "../../../../AxiosContigForTicket";
 
 const Resolved = () => {
     const [tickets, setTickets] = useState([]);
     const { loading, startLoading, stopLoading } = useLoading();
     const navigate = useNavigate();
 
-    // Fetch tickets from the API
-    useEffect(() => {
-        startLoading(); // Start loading indicator
-        const fetchTickets = async () => {
-            try {
-                const response = await fetch("http://13.203.24.247:2000/api/v1/ticket?status=Resolved");
-                const data = await response.json();
-                setTickets(data);
-            } catch (error) {
-                // stopLoading();
-                console.error("Error fetching tickets:", error);
-            } finally {
-                stopLoading(); // Stop loading regardless of success or error
+    const fetchTickets = async () => {
+        try {
+            startLoading(); 
+            const userId = localStorage.getItem('userId');
+            let userData = {}
+            await axiosInstance.get(`adminAuth/getSuperAdmin/${userId}`)
+                .then(response => {
+                    userData = response.data.user
+                })
+                .catch(error =>{
+                    console.log(error);
+                })
+                console.log('userData-',userData);
+            let response
+            if(userData.userType === "Employee"){
+                response = await axiosInstanceForTicket(`ticket?userId=${userId}&status=Resolved`);
             }
-        };
+            if(!response){
+                response = await axiosInstanceForTicket(`ticket?status=Resolved`);
+            }
+            setTickets(response.data.tickets);
+        } catch (error) {
+          console.error("Error fetching tickets:", error);
+        } finally {
+          stopLoading();
+        }
+    };
+
+    useEffect(() => {
         fetchTickets();
     }, []);
 
@@ -48,8 +64,14 @@ const Resolved = () => {
                             <div className="d-flex align-items-center gap-2">
                                 <div className="view-ticket-circle" style={{ backgroundColor: ticket.priority === "High" ? "#F8A534" : ticket.priority === "Medium" ? "#3B8AFF" : "#54C104" }} />
                                 <div className="allTickets-ticket-title">Ticket# {ticket.ticketNumber}</div>
-                                {ticket.priority && <div className="allTickets-priority-p">{ticket.priority} Priority</div>}
-                            </div>
+                                {ticket.priority && 
+                                  ticket.priority === "Low" ? 
+                                    <div className="allTickets-low-priority-p">{ticket.priority} Priority</div>
+                                  : ticket.priority === "Medium" ? 
+                                    <div className="allTickets-medium-priority-p">{ticket.priority} Priority</div>
+                                    :
+                                  <div className="allTickets-high-priority-p">{ticket.priority} Priority</div>
+                                }                            </div>
                             <div>Posted at {new Date(ticket.createdAt).toLocaleTimeString()}</div>
                         </div>
                         <div className="mt-2">
@@ -62,7 +84,7 @@ const Resolved = () => {
                                 <img src={john_img} alt="" style={{ width: "30px" }} />
                                 <p className="allTickets-p mb-0">{ticket.createdBy.username}</p>
                             </div>
-                            <button className="allTickets-openticket-button" onClick={() => handleClick(ticket._id)}> Open Ticket </button>
+                            <button className="allTickets-closedticket-button" onClick={() => handleClick(ticket._id)}> Resolved Ticket </button>
                         </div>
                     </div>
                 ))
