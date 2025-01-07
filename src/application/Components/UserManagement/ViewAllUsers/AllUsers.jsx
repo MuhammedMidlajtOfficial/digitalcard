73d952744da95ac29951f524fff281522c8f47ca
Manjulatha10 <amanjulatha.levontechno@gmail.com>
2025-01-07 14:50:07 +0530

    Working on View user profile

--- src/application/Components/UserManagement/ViewAllUsers/AllUsers.jsx
+++ src/application/Components/UserManagement/ViewAllUsers/AllUsers.jsx
@@ -1,16 +1,35 @@
 import React, { useState } from "react";
 import UsersCards from "./UsersCards";
 import UsersProfiles from "./UsersProfiles";
+
 const AllUsers = () => {
-  const [change,setChange] = useState(true)
+  const [change, setChange] = useState(true);
+  const [filter, setFilter] = useState("individualUser"); 
+
   return (
     <div className="application-all-users-section">
       <div className="row">
-        <UsersCards change={change}/>
-        <UsersProfiles setChange={setChange}/>
+      {/* <UsersCards change={setChange} activeFilter={filter} /> */}
+        <UsersProfiles setChange={setChange} setFilter={setFilter} />
       </div>
     </div>
   );
 };
 
 export default AllUsers;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
