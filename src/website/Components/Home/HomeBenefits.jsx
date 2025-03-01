d51a3845e1ce9e58cef1b04f9606194432251cbe
manjuajgowda <152592250+manjuajgowda@users.noreply.github.com>
2025-03-01 11:52:22 +0530

    worked on images

--- src/website/Components/Home/HomeBenefits.jsx
+++ src/website/Components/Home/HomeBenefits.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import individual from '../../Assets/image/home/individual-benefit.svg';
-import enterprise from '../../Assets/image/home/enterprize-benefit.svg';
+import enterprise from '../../Assets/image/home/enterprize-benefit1.svg';
 import ind from "../../Assets/image/home/Icons/ind.svg";
 import ent from "../../Assets/image/home/Icons/ent.svg";
 import ScrollAnimation from '../../../ScrollAnimation';
